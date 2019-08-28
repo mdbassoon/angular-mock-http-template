ed5d654a84e1e01df0f458d6af7981994db434c7
mdbassoon <mark.delgado87@gmail.com>
2019-08-28 13:12:59 -0700

    Added a description in the .readme

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.1.
 
+## Description
+
+This is a template designed to provide a quick starting point to prototype more quickly without a backend with the intention of eventually replacing the backend with http requests to a server. To modify the starting obj, look under src/http-obj.service.ts and copy in your current working object before refreshing.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HttpObjService } from './http-obj.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'starter-mock-http';
+  constructor(private objService:HttpObjService){};
+  title = 'project-manager';
+  /*  API */
+  obj:object;
+  keys = new Array();
+  getObj() {
+    const newObject = this.objService.get('url-placeholder');
+    this.obj = newObject;
+    this.keys = Object.keys(newObject);
+  }
+  postObj() {
+    return this.objService.post(this.obj);
+  }
+  changeParam(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    let obj = this.obj;
+    obj[name] = value;
+    this.obj = obj;
+    this.postObj();
+    this.getObj();
+    console.log(this.obj);
+  }
+  ngOnInit() {
+    this.getObj();
+  }
 }
